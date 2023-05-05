9969d6208ec5fa6ee466b12307699d4399d43bb1
Zaibonisha <zsmohamed94@gmail.com>
2023-05-05 19:49:55 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -29,18 +29,34 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    return <Graph data={this.state.data} />;
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Stop requesting data from server when it returns no data
+        if (serverResponds.length === 0) {
+          clearInterval(interval);
+        }
+
+        // Update the state by creating a new array of data that consists of
+        // Previous data in the state and the new data from server
+        this.setState((prevState) => ({
+          data: [...prevState.data, ...serverResponds],
+        }));
+      });
+
+      x++;
+      // Stop requesting data after 1000 requests
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
@@ -49,26 +65,28 @@ class App extends Component<{}, IState> {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          Bank & Merge Co Task 2
-        </header>
+        <header className="App-header">Bank & Merge Co Task 2</header>
         <div className="App-content">
-          <button className="btn btn-primary Stream-button"
-            // when button is click, our react app tries to request
-            // new data from the server.
-            // As part of your task, update the getDataFromServer() function
-            // to keep requesting the data every 100ms until the app is closed
-            // or the server does not return anymore data.
-            onClick={() => {this.getDataFromServer()}}>
+          <button
+            className="btn btn-primary Stream-button"
+            onClick={() => {
+              this.getDataFromServer();
+            }}
+          >
             Start Streaming Data
           </button>
           <div className="Graph">
-            {this.renderGraph()}
+            {this.state.data.length > 0 ? (
+              this.renderGraph()
+            ) : (
+              <div className="no-data">No data available</div>
+            )}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 export default App;
+
